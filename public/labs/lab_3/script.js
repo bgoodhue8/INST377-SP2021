af71142e017496721847cba31d7725f239bf2a31
bgoodhue8 <6305+bgoodhue8@users.noreply.github.com>
2021-05-11 05:03:14 -0400

    source(https://codepen.io/bramus/pen/VwpwdZy)

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,17 +1,1 @@
 /* Put your javascript in here */
-let width = 50;
-let count = 3;
-let slider = carousel.querySelector('div');
-let slide = carousel.querySelectorAll('img');
-
-carousel.querySelector('.prev').onclick = function() {
-    position += width * count;
-    position = Math.min(position, 0)
-    slider.style.marginLeft = position + 'px';
-};
-
-carousel.querySelector('.next').onclick = function() {
-    position -= width * count;
-    position = Math.max(position, -width * (slide.length - count));
-    slider.style.marginLeft = position + 'px';
-};
