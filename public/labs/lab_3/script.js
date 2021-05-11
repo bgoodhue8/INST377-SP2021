5297410ed641aaf9e00781f8fccce7c8bcb79907
bgoodhue8 <6305+bgoodhue8@users.noreply.github.com>
2021-05-11 00:56:10 -0400

    lab 3 almost done

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,17 @@
 /* Put your javascript in here */
+let width = 50;
+let count = 3;
+let slider = carousel.querySelector('a');
+let item = carousel.querySelectorAll('img');
+
+carousel.querySelector('.prev').onclick = function() {
+    position += width * count;
+    position = Math.min(position, 0)
+    slider.style.marginLeft = position + 'px';
+};
+
+carousel.querySelector('.next').onclick = function() {
+    position -= width * count;
+    position = Math.max(position, -width * (item.length - count));
+    slider.style.marginLeft = position + 'px';
+};
